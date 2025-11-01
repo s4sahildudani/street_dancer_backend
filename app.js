e57824a4af8181bd3b05475951036ddc43ea999a
sahil dudani <s4sahil.dudani@gmail.com>
2025-11-01 23:43:56 +0530

    changed routes of app

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Routes (keep your existing route files)
-app.use('/users', require('./routes/users'));
-app.use('/instructors', require('./routes/instructors'));
-app.use('/dancedata', require('./routes/dancedata'));
-app.use('/danceclasses', require('./routes/danceclasses'));
-app.use('/danceforms', require('./routes/danceforms'));
+app.use('/users', require('../routes/users'));
+app.use('/instructors', require('../routes/instructors'));
+app.use('/dancedata', require('../routes/dancedata'));
+app.use('/danceclasses', require('../routes/danceclasses'));
+app.use('/danceforms', require('../routes/danceforms'));
 
 module.exports = app;
